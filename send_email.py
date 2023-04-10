b09cc8ae86a53abe8cfcf20eb5c3abbf81bd5a41
oksen <oksenspiwak10@gmail.com>
2023-04-10 15:19:17 +0200

    Send email correctly

--- send_email.py
+++ send_email.py
@@ -1,20 +1,15 @@
 import smtplib
 import ssl
 
-host = 'smtp.gmail.com'
-port = 465
 
-username = 'oksens.company@gmail.com'
-password = 'qnuktwacsilblgoh'
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
+    username = 'oksens.company@gmail.com'
+    password = 'qnuktwacsilblgoh'
+    receiver = 'oksens.company@gmail.com'
+    context = ssl.create_default_context()
 
-receiver = 'oksens.company@gmail.com'
-context = ssl.create_default_context()
-
-message = '''\
-Subject: Wazne informacje na temat pedofilii w kosciele
-How are you?
-Bye
-'''
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message.encode('utf-8'))
