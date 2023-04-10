1b5b6f6821ae446ba2d361ddd46e2748b95a73e8
oksen <oksenspiwak10@gmail.com>
2023-04-10 10:37:38 +0200

    pandas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas
 st.set_page_config(layout='wide')
 
 col1, col2 = st.columns(2)
@@ -12,3 +12,16 @@ with col2:
     content = '''Filantrop, kulturysta, entuzjasta ciepłego ciasta
     oraz kandydat na kandytata.(Jakaś suka potraktowała mnie gazem pieprzowym)'''
     st.info(content)
+
+st.write("Co tam słychać? Stare kurwy nie chcą zdychac")
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv('data.csv', sep=';')
+with col3:
+    for index, row in df[10:].iterrows():
+        st.header(row['title'])
+
+with col4:
+    for index, row in df[:10].iterrows():
+        st.header(row['title'])
